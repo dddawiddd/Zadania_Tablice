9e8c769debab72dd0f9dc561ea2eefc3ac9a0eeb
dddawiddd <dawidn@onet.eu>
2019-03-30 13:49:18 +0100

    1

--- src/Zad6.java
+++ src/Zad6.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 
 public class Zad6 {
 
-
     public static void main(String[] args) {
 
-        String[][] kolkokrzyzyk = new String[3][3];                         //[wiersze][kolumny]
-        Scanner scaner = new Scanner(System.in);
+        String[][] kolkokrzyzyk = new String[4][4];                         //[wiersze][kolumny]
 
         for (int k = 0; k < kolkokrzyzyk.length; k++) {                      //nadpisywanie
             for (int l = 0; l < kolkokrzyzyk[0].length; l++) {
@@ -14,82 +12,84 @@ public class Zad6 {
             }
         }
 
-        int wiersz, kolumna;
-
-
-
-
         System.out.println("GRA KOLKO KRZYRZYK\n");
 
-        label:
         while (true) {
 
-            System.out.println("Gracz: KOLKO");
+            wprowadzanie(kolkokrzyzyk, "O", "KOLKO");
 
-            System.out.print("Wprowadz wiersz [1-3]: ");
-            wiersz = scaner.nextInt() - 1;
-            System.out.print("Wprowadz kolumne [1-3]: ");
-            kolumna = scaner.nextInt() - 1;
+            drukowanie(kolkokrzyzyk);                                                    //drukowanie O
 
-            if (wiersz < kolkokrzyzyk.length && wiersz >= 0 && kolumna < kolkokrzyzyk[0].length && kolumna >= 0) {
-                if (kolkokrzyzyk[wiersz][kolumna].equals(" "))
-                    kolkokrzyzyk[wiersz][kolumna] = "O";
-            }
-
-            for (int i = 0; i < kolkokrzyzyk.length; i++) {                      //drukowanie  todo pzrobić z drukowania metode
-                for (int j = 0; j < kolkokrzyzyk[0].length; j++) {
-                    System.out.print("|" + kolkokrzyzyk[i][j] + "|");
-                }
-                System.out.println("");
-            }
-
-            for (int p = 0; p < kolkokrzyzyk.length; p++) {                       //sprawdzanie wygranej
-
-                if (kolkokrzyzyk[p][0].equals("O") && kolkokrzyzyk[p][1].equals("O") && kolkokrzyzyk[p][2].equals("O") || kolkokrzyzyk[0][p].equals("O") && kolkokrzyzyk[1][p].equals("O") && kolkokrzyzyk[2][p].equals("O")) {
-                    System.out.println("Kołko WYGRAŁ !!!");
-                    break label;          //todo powinno przerwać while
-                }
-            }
-
-            if (kolkokrzyzyk[0][0].equals("O") && kolkokrzyzyk[1][1].equals("O") && kolkokrzyzyk[2][2].equals("O") || kolkokrzyzyk[0][2].equals("O") && kolkokrzyzyk[1][1].equals("O") && kolkokrzyzyk[2][0].equals("O")) {
-                System.out.println("Kołko WYGRAŁ !!!");
+            if (spradzanieWygranej(kolkokrzyzyk, "O", "KOLKO")) {                //sprawdzanie O
                 break;
             }
 
 
+            wprowadzanie(kolkokrzyzyk, "X", "KRZYZYK");
 
+            drukowanie(kolkokrzyzyk);                                                    //drukowanie X
 
-
-            System.out.println("Gracz: KRZYZYK");
-
-            System.out.print("Wprowadz wiersz [1-3]: ");
-            wiersz = scaner.nextInt() - 1;
-            System.out.print("Wprowadz kolumne [1-3]: ");
-            kolumna = scaner.nextInt() - 1;
-
-            if (wiersz < kolkokrzyzyk.length && wiersz >= 0 && kolumna < kolkokrzyzyk[0].length && kolumna >= 0) {
-                if (kolkokrzyzyk[wiersz][kolumna].equals(" "))
-                    kolkokrzyzyk[wiersz][kolumna] = "X";
-            }
-
-            for (int i = 0; i < kolkokrzyzyk.length; i++) {                      //drukowanie  todo pzrobić z drukowania metode
-                for (int j = 0; j < kolkokrzyzyk[0].length; j++) {
-                    System.out.print("|" + kolkokrzyzyk[i][j] + "|");
-                }
-                System.out.println("");
-            }
-
-            for (int p = 0; p < kolkokrzyzyk.length; p++) {                       //sprawdzanie wygranej
-                if (kolkokrzyzyk[p][0].equals("X") && kolkokrzyzyk[p][1].equals("X") && kolkokrzyzyk[p][2].equals("X") || kolkokrzyzyk[0][p].equals("X") && kolkokrzyzyk[1][p].equals("X") && kolkokrzyzyk[2][p].equals("X")) {
-                    System.out.println("Kołko WYGRAŁ !!!");
-                    break label;
-                }
-            }
-
-            if (kolkokrzyzyk[0][0].equals("X") && kolkokrzyzyk[1][1].equals("X") && kolkokrzyzyk[2][2].equals("X") || kolkokrzyzyk[0][2].equals("X") && kolkokrzyzyk[1][1].equals("X") && kolkokrzyzyk[2][0].equals("X")) {
-                System.out.println("Kołko WYGRAŁ !!!");
+            if (spradzanieWygranej(kolkokrzyzyk, "X", "KRZYZYK")) {                //sprawdzanie X
                 break;
             }
         }
+    }
+
+
+    private static void wprowadzanie(String[][] kolkokrzyzyk, String ox, String who) {
+
+        Scanner scaner = new Scanner(System.in);
+        int wiersz, kolumna;
+
+        System.out.println("Gracz: " + who);
+
+        while (true) {
+            System.out.print("Wprowadz wiersz [1-" + kolkokrzyzyk.length + "]: ");
+            //wiersz = Integer.parseInt(scaner.nextLine())-1;
+            wiersz = scaner.nextInt() - 1;
+            System.out.print("Wprowadz kolumne [1-" + kolkokrzyzyk[0].length + "]: ");
+            kolumna = scaner.nextInt() - 1;
+
+
+            if (wiersz < kolkokrzyzyk.length && wiersz >= 0 && kolumna < kolkokrzyzyk[0].length && kolumna >= 0) {
+                if (kolkokrzyzyk[wiersz][kolumna].equals(" ")) {
+                    kolkokrzyzyk[wiersz][kolumna] = ox;
+                    break;
+                }
+                System.out.println("To pole jest zajete. " + who + " wprowadz jeszcze raz inne pole");
+            } else
+                System.out.println("Nie ma takiego pola. Podaj jeszcze raz.");
+        }
+    }
+
+
+    public static void drukowanie(String[][] kolkokrzyzyk) {
+
+        for (String[] strings : kolkokrzyzyk) {                      //drukowanie  todo pzrobić z drukowania metode
+            for (int j = 0; j < kolkokrzyzyk[0].length; j++) {
+                System.out.print("|" + strings[j] + "|");
+            }
+            System.out.println("");
+        }
+    }
+
+
+    public static boolean spradzanieWygranej(String[][] kolkokrzyzyk, String ox, String who) {
+
+        for (int p = 0; p < kolkokrzyzyk.length; p++) {
+
+            if (kolkokrzyzyk[p][0].equals(ox) && kolkokrzyzyk[p][1].equals(ox) && kolkokrzyzyk[p][2].equals(ox) || kolkokrzyzyk[0][p].equals(ox) && kolkokrzyzyk[1][p].equals(ox) && kolkokrzyzyk[2][p].equals(ox)) {
+                System.out.println(who + " WYGRAŁ !!!");
+                //break;          //todo powinno przerwać while
+                return true;
+            }
+        }
+
+        if (kolkokrzyzyk[0][0].equals(ox) && kolkokrzyzyk[1][1].equals(ox) && kolkokrzyzyk[2][2].equals(ox) || kolkokrzyzyk[0][2].equals(ox) && kolkokrzyzyk[1][1].equals(ox) && kolkokrzyzyk[2][0].equals(ox)) {
+            System.out.println(who + " WYGRAŁ !!!");
+            //break;
+            return true;
+        }
+        return false;
     }
 }
